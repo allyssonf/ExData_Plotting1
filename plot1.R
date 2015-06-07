a2bb8b0ee314bf58f94438e06b3b0b35889eb0b4
Allysson Oliveira <allysson.oliveira@gmail.com>
2015-06-06 21:53:29 -0300

    Changes to plot2.R. Subsetting data to plot but still need to fix the correct approach

--- plot1.R
+++ plot1.R
@@ -8,9 +8,10 @@ library(sqldf)
 
 # The assumption here is that the data set file is at the same directory of the script
 epcds <- read.csv.sql(file = "household_power_consumption.txt", 
-                      sql = "select * from file where Date = '2/2/2007' or Date = '1/2/2007'", 
+                      sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'", 
                       colClasses = c("character", "character", "numeric", "numeric", "numeric",
                                      "numeric", "numeric", "numeric", "numeric"), sep = ";")
+
 
 # Now that we have the data, lets plot!
 hist(epcds$Global_active_power, col = "red")
--- plot2.R
+++ plot2.R
@@ -6,11 +6,24 @@ png(filename = "plot2.png", width = 480, height = 480)
 # Load sqldf library to use read.csv.sql to avoid high memory comsumption
 library(sqldf)
 
+# Load GGPlot2 library to plot #2 image.
+library(ggplot2)
+
+# Sorry but I need this as my locale is pt_BR
+daysinweek <- c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+
 # The assumption here is that the data set file is at the same directory of the script
 epcds <- read.csv.sql(file = "household_power_consumption.txt", 
-                      sql = "select * from file where Date = '2/2/2007' or Date = '1/2/2007'", 
+                      sql = "select * from file where Date = '1/2/2007' or Date = '2/2/2007'", 
                       colClasses = c("character", "character", "numeric", "numeric", "numeric",
                                      "numeric", "numeric", "numeric", "numeric"), sep = ";")
+
+
+# Get correct days of week. First day is Sunday and it is equals 0.
+daycolumn <- daysinweek[as.POSIXlt(as.Date(epcds$Date, "%d/%m/%Y"))$wday + 1]
+
+# Now just get a correct set with correct columns
+sset <- cbind(daycolumn, epcds[,c(2:9)])
 
 # Now that we have the data, lets plot!
 hist(epcds$Global_active_power, col = "red")
